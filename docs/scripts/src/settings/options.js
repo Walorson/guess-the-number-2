9c23d3dd628bb5eca00ff47e6b9432140a7ec40d
Walorson <zembakii@gmail.com>
2024-09-07 17:13:12 +0200

    new skins

--- docs/scripts/src/settings/options.js
+++ docs/scripts/src/settings/options.js
@@ -3,8 +3,8 @@ import {changeColor, changeBackground, changeFont} from "../theme.js";
 export let options;
 window.addEventListener("load", () => {
   options = [
-    new CustomSettingMultiple("Color", "red", "options", ["red", "blue", "green", "yellow", "purple"], changeColor),
-    new CustomSettingMultiple("Background", "default", "options", ["default", "memphis", "carpet", "marble", "wood", "none"], changeBackground),
-    new CustomSettingMultiple("Font", "default", "options", ["default", "comic sans", "jetbrains mono", "old london", "horror", "minecraft"], changeFont)
+    new CustomSettingMultiple("Color", "red", "options", ["red", "blue", "green", "yellow", "purple", "mint", "brown"], changeColor),
+    new CustomSettingMultiple("Background", "default", "options", ["default", "memphis", "carpet", "marble", "wood", "minecraft", "none"], changeBackground),
+    new CustomSettingMultiple("Font", "default", "options", ["default", "comic sans", "jetbrains mono", "old london", "minecraft", "super mario", "horror", "capture it"], changeFont)
   ];
 });
--- src/scripts/src/theme.ts
+++ src/scripts/src/theme.ts
@@ -3,7 +3,7 @@ export function changeColor(color: string): void
     switch(color)
     {
         case "red": {
-            colorPalette("217, 29, 29", "182, 23, 23", "137, 16, 16", "175, 21, 21", "137,16,16, 0.75", "255, 0, 0");
+            colorPalette("", "", "", "", "");
         } break;
         case "blue": {
             colorPalette("15, 161, 194","24, 177, 219","4, 121, 164","21, 118, 175","11, 65, 104, 0.75", "23, 152, 166");
@@ -17,17 +17,35 @@ export function changeColor(color: string): void
         case "purple": {
             colorPalette("95, 42, 171", "122, 28, 216", "87, 20, 163", "115, 25, 190", "79, 38, 194, 0.5", "113, 28, 162");
         } break;
+        case "mint": {
+            colorPalette("28, 199, 133", "19, 204, 140", "15, 173, 120", "14, 138, 97", "20, 169, 117, 0.5", "19, 204, 140");
+        } break;
+        case "brown": {
+            colorPalette("143, 98, 56", "150, 101, 62", "112, 72, 44", "112, 77, 36", "123, 85, 47, 0.5", "144, 99, 65");
+        } break;
     }
 }
 
 function colorPalette(colorMain: string, colorTitle: string, colorBorder: string, colorShadow: string, colorShadow2: string, progressCircle: string = "red"): void
 {
-    document.documentElement.style.setProperty("--color-main", "rgb("+colorMain+")");
-    document.documentElement.style.setProperty("--color-title", "rgb("+colorTitle+")");
-    document.documentElement.style.setProperty("--color-border", "rgb("+colorBorder+")");
-    document.documentElement.style.setProperty("--color-shadow", "rgb("+colorShadow+")");
-    document.documentElement.style.setProperty("--color-shadow2", "rgba("+colorShadow2+")");
-    document.documentElement.style.setProperty("--color-progressCircle", "rgb("+progressCircle+")");
+    if(colorMain == "")
+    {
+        document.documentElement.style.setProperty("--color-main", "");
+        document.documentElement.style.setProperty("--color-title", "");
+        document.documentElement.style.setProperty("--color-border", "");
+        document.documentElement.style.setProperty("--color-shadow", "");
+        document.documentElement.style.setProperty("--color-shadow2", "");
+        document.documentElement.style.setProperty("--color-progressCircle", "");
+    }
+    else
+    {
+        document.documentElement.style.setProperty("--color-main", "rgb("+colorMain+")");
+        document.documentElement.style.setProperty("--color-title", "rgb("+colorTitle+")");
+        document.documentElement.style.setProperty("--color-border", "rgb("+colorBorder+")");
+        document.documentElement.style.setProperty("--color-shadow", "rgb("+colorShadow+")");
+        document.documentElement.style.setProperty("--color-shadow2", "rgba("+colorShadow2+")");
+        document.documentElement.style.setProperty("--color-progressCircle", "rgb("+progressCircle+")");
+    }
 }
 
 export function changeBackground(name: string): void
